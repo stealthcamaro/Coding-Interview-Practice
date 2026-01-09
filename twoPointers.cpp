8657ae6e3ef62d75feedf3952c6262ae93f24df7
stealthcamaro <lujanuriel@icloud.com>
2026-01-09 13:01:52 -0600

    Adding the solution to a problem where two pointers can be used to solve for example a string that is or isnt a palindrome.

---
+++ twoPointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrone(string s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        // If the characters don't match, it's not a palindrome
+        if(s[left] != s[right]) {
+            return false; // not a Palindrone
+        }
+        // Move the pointers inward, closer to each other
+        left++;
+        right--;
+    }
+    return true; // Assuming the conditions of the loop are true,
+                 // the Input IS a Palindrome
+}
+
+int main(){
+
+    return 0;
+}
