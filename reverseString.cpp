96426b4b3a2f5244d26164ec9fc8022adfa5a64e
stealthcamaro <lujanuriel@icloud.com>
2026-01-10 00:37:28 -0600

    Analyzed the logic behind the code and it's just a simple swap using a built in C++ function. Very simple but efficient.

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,3 +1,11 @@
+// This solution is for any problem that may require reversing a string.
+// It reverses a string simply by switching Left and Right using the swap
+// built in function in C++. No second array or string was needed so this
+// solution is very space efficient
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
 #include <vector>
 #include <algorithm> // for std::swap
 
@@ -8,10 +16,15 @@ void reverseString(vector<char> &s) {
     int right = s.size() - 1;
 
     while (left < right) {
-        // The "Action" step
+        // The "Action" step (The Swap)
         swap(s[left], s[right]); // Using built-in C++ function to trade places
 
-        // The "Movement" step
+        // The "Movement" step where the while loop moves inward.
+        // The "left" integer moves to the right and the "right"
+        // integer moves to the left until they meet in the middle
+        // this pincer movement is very similar to the "Two Pointers"
+        // solution that is used for the Palindrome problem except these
+        // are integers to access char data at an array index.
         left++;
         right--;
     }
